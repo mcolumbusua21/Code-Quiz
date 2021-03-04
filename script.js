e5ac1e5f1d2a57a215543ce3cd26d52e3cd4ee01
Mark Columbus <mcolumbusua21@gmail.com>
2021-03-03 18:11:28 -0700

    timer

--- script.js
+++ script.js
@@ -7,39 +7,53 @@ var questionTitle = document.querySelector("#questions-text")
 var answerContainer = document.querySelector("#answers")
 var currentIndex = 0
 
-function startGame() {
-  timerCount = 80;
-}
 
 var questions = [
-  { question: "Is the sky blue?", options: ["True", "False"], answers: "True"},
-
-  { question: "What do dogs eat?",
+    { question: "Is the sky blue?", options: ["True", "False"], answers: "True"},
+    
+    { question: "What do dogs eat?",
     options: ["Dog food", "Cat food", "Pasta", "Pack rats"],
     answers: "Dog food",
-  },
-  { question: "How long is class?",
-    options: ["3 1/2 hours", "2 hours", "14 hours", "4 1/2 hours"],
+},
+{ question: "How long is class?",
+options: ["3 1/2 hours", "2 hours", "14 hours", "4 1/2 hours"],
     answers: "4 1/2hours",
   },
-
+  
   { question: "Is Tucson in California?",
-    options: ["True", "False"],
+  options: ["True", "False"],
     answers: "False",
-  },
+},
 
-  { question: "How many pets do I have?",
-    options: ["3", "2", "0", "5"],
-    answers: "0",
-  },
+{ question: "How many pets do I have?",
+options: ["3", "2", "0", "5"],
+answers: "0",
+},
 ];
 
-startBtn.addEventListener("click", function (e) {
-    // e.preventDefault();
-  startPrompt.style.display = "none";
-  timer.style.display = "block";
- renderQuestion()
-});
+
+function timerStart (){
+    var count = 80;
+    var interval = setInterval(timerFunction, 1000);
+    function timerFunction (){
+        if (count ===0){
+            clearInterval(interval);
+            document.getElementById("timer-count").text("Times Up")
+            return;
+        }
+        --count;
+        document.getElementById("timer-count").text(count)
+    }
+}
+
+  startBtn.addEventListener("click", function (e) {
+      // e.preventDefault();
+      timerStart()
+      startPrompt.style.display = "none";
+      timer.style.display = "block";
+      renderQuestion()
+    }
+);
 
 function renderQuestion(){
     questionTitle.innerHTML= questions[currentIndex].question;
@@ -50,6 +64,7 @@ function renderQuestion(){
        answerContainer.append(btn)
     }
 }
+
 
 //make function to handle clicking an answer button
 //in that function, evaluate if true/false
